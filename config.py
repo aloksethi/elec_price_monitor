dd27bec50f8095ba3137777be0099a64bb1858a0
alsethi <2630809+aloksethi@users.noreply.github.com>
2025-06-28 01:22:04 +0300

    config.py: added variables for udp server

--- config.py
+++ config.py
@@ -1,11 +1,14 @@
 DEBUG = True
 MOCK_REST_DATA = False
 
-SLEEP_DUR_NO_DATA = 5
-SLEEP_DUR_NO_TMRW_DATA = 30*60
-SLEEP_DUR_DATA_AVLBL = 2*60*60
+SLEEP_DUR_NO_DATA:int = 5
+SLEEP_DUR_NO_TMRW_DATA:int = 30*60
+SLEEP_DUR_DATA_AVLBL:int = 2*60*60
 
 if DEBUG and MOCK_REST_DATA:
     BASE_URL = "http://localhost:5000/api"
 else:
     BASE_URL = "https://web-api.tp.entsoe.eu/api"
+
+PY_PORT = 6666 #udp port for the server running at python
+UC_PORT = 6667 #udp port for the server running at uC
