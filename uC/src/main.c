ddb39bb6a98a575cdee0bb150776d200066a0be9
asethi <aloksethi@users.noreply.github.com>
2025-07-09 22:58:21 +0300

    well, have to create a task for cyw43 otherwise it crashes.juste ran iperf client. also debug is working.

--- uC/src/main.c
+++ uC/src/main.c
@@ -14,11 +14,13 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
+#define CLIENT_TEST 	1
+#define IPERF_SERVER_IP		"10.10.10.231"
 #ifndef RUN_FREERTOS_ON_CORE
 #define RUN_FREERTOS_ON_CORE 0
 #endif
 
-#define TEST_TASK_PRIORITY				( tskIDLE_PRIORITY + 2UL )
+#define MAIN_TASK_PRIORITY				( tskIDLE_PRIORITY + 2UL )
 #define BLINK_TASK_PRIORITY				( tskIDLE_PRIORITY + 1UL )
 
 #if CLIENT_TEST && !defined(IPERF_SERVER_IP)
@@ -37,6 +39,8 @@ static void iperf_report(void *arg, enum lwiperf_report_type report_type,
 
     printf("Completed iperf transfer of %d MBytes @ %.1f Mbits/sec\n", mbytes, mbits);
     printf("Total iperf megabytes since start %d Mbytes\n", total_iperf_megabytes);
+
+    return;
 }
 
 void blink_task(__unused void *params) {
@@ -52,25 +56,33 @@ void blink_task(__unused void *params) {
 #endif
         cyw43_arch_gpio_put(0, on);
         on = !on;
-        vTaskDelay(200);
+        vTaskDelay(1000);
+	//printf("blinking now from core %d\n", portGET_CORE_ID());
     }
 }
 
+void create_tasks(void)
+{
+
+	return;	
+}
 void main_task(__unused void *params) {
     if (cyw43_arch_init()) {
         printf("failed to initialise\n");
+        exit(1);
         return;
     }
     cyw43_arch_enable_sta_mode();
     printf("Connecting to Wi-Fi...\n");
     if (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 30000)) {
-        printf("failed to connect.\n");
-	printf("%s\n", WIFI_PASSWORD);
-	printf("%s\n", WIFI_SSID);
+        printf("failed to connect to Wi-Fi.\n");
         exit(1);
-    } else {
-        printf("Connected.\n");
-    }
+    } 
+
+    printf("Connected to the Wi-Fi.\n");
+   
+
+    create_tasks();
 
     xTaskCreate(blink_task, "BlinkThread", configMINIMAL_STACK_SIZE, NULL, BLINK_TASK_PRIORITY, NULL);
 
@@ -78,7 +90,8 @@ void main_task(__unused void *params) {
 #if CLIENT_TEST
     printf("\nReady, running iperf client\n");
     ip_addr_t clientaddr;
-    ip4_addr_set_u32(&clientaddr, ipaddr_addr(xstr(IPERF_SERVER_IP)));
+    ip4_addr_set_u32(&clientaddr, ipaddr_addr((IPERF_SERVER_IP)));
+    //ip4_addr_set_u32(&clientaddr, ipaddr_addr(xstr(IPERF_SERVER_IP)));
     assert(lwiperf_start_tcp_client_default(&clientaddr, &iperf_report, NULL) != NULL);
 #else
     printf("\nReady, running iperf server at %s\n", ip4addr_ntoa(netif_ip4_addr(netif_list)));
@@ -94,19 +107,6 @@ void main_task(__unused void *params) {
     cyw43_arch_deinit();
 }
 
-void vLaunch( void) {
-
-#if NO_SYS && configUSE_CORE_AFFINITY && configNUMBER_OF_CORES > 1
-    // we must bind the main task to one core (well at least while the init is called)
-    // (note we only do this in NO_SYS mode, because cyw43_arch_freertos
-    // takes care of it otherwise)
-#error("coming here")
-    vTaskCoreAffinitySet(task, 1);
-#endif
-
-    /* Start the tasks and timer running. */
-    vTaskStartScheduler();
-}
 
 int main( void )
 {
@@ -114,7 +114,8 @@ int main( void )
     stdio_init_all();
 
 
-    xTaskCreate(main_task, "TestMainThread", configMINIMAL_STACK_SIZE, NULL, TEST_TASK_PRIORITY, &task);
-    vLaunch();
+    xTaskCreate(main_task, "MainThread", configMINIMAL_STACK_SIZE, NULL, MAIN_TASK_PRIORITY, &task);
+
+    vTaskStartScheduler();
     return 0;
 }
