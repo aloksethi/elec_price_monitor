056d9c6ff7a258cd3ba19958f6d0f648eed577f4
asethi <aloksethi@users.noreply.github.com>
2025-07-27 21:10:26 +0300

    added the epaper operation back

--- uC/src/epaper_handler.c
+++ uC/src/epaper_handler.c
@@ -12,32 +12,33 @@
 
 void epaper_task(void *params) 
 {
-//	memset(blk_buf, 0xffffffff, DISP_BUFF_SIZE/sizeof(uint32_t));
-//	memset(red_buf, 0xffffffff, DISP_BUFF_SIZE/sizeof(uint32_t));
+    //	memset(blk_buf, 0xffffffff, DISP_BUFF_SIZE/sizeof(uint32_t));
+    //	memset(red_buf, 0xffffffff, DISP_BUFF_SIZE/sizeof(uint32_t));
     epaper_gpios_init();
 
     while (1) {
         // TODO: Wait for signal, decompress buffer, update display
-//        vTaskDelay(pdMS_TO_TICKS(2000));
+        //        vTaskDelay(pdMS_TO_TICKS(2000));
         ulTaskNotifyTake( pdTRUE, portMAX_DELAY );
-        printf("epaper started working\n");
-       for (uint16_t i=0;i<DISP_BUFF_SIZE;i++)
-       {
+        UC_DEBUG("epaper started working\n");
+        for (uint16_t i=0;i<DISP_BUFF_SIZE;i++)
+        {
             bimg_buf[i] = ~bimg_buf[i];
             rimg_buf[i] = ~rimg_buf[i];
             //rimg_buf[i] &= ~bimg_buf[i];
 
-       }
+        }
+#if 1
         EPD_5IN83B_V2_Init();
         EPD_5IN83B_V2_Clear();
-        
+
         DEV_Delay_ms(500);
         //EPD_5IN83B_V2_Display(bimg_buf, rimg_buf);
 
         EPD_5IN83B_V2_Display(rimg_buf, bimg_buf);
         DEV_Delay_ms(2000);
         EPD_5IN83B_V2_Sleep();
-
+#endif
 
 
         xEventGroupSync(g_sleep_eg, SLEEP_EG_EPAPER_DONE_BIT, ALL_SYNC_BITS, portMAX_DELAY );
